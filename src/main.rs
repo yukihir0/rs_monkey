56406810d3b740c2fa1107649c6f3fd430620c5c
yukihir0 <yukihiro.cotori@gmail.com>
2018-09-05 21:53:10 +0900

    add 1.4 4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 mod token;
 mod lexer;
 
+use std::io::{self, BufRead, Write};
+use token::Token;
+use lexer::Lexer;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+
+    loop {
+        print!(">> ");
+        io::stdout().flush().expect("Error flushing stdout");
+
+        let mut line = String::new();
+        stdin.lock().read_line(&mut line).expect("Error reading from stdin");
+        let mut lexer = Lexer::new(&mut line);
+
+        loop {
+            let tok = lexer.next_token();
+            println!("{:?}", tok);
+            if tok == Token::EOF{
+                break;
+            }
+        }
+    }
 }
